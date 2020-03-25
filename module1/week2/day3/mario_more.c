4a4b57eae6c36a0f60bfe5bbb328cf73c16c25e1
unknown <wilber.carrascal@gmail.com>
2020-03-25 15:46:39 -0500

    finish_mario

---
+++ module1/week2/day3/mario_more.c
@@ -0,0 +1,34 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int cant_rows = 0;
+    while (!cant_rows || cant_rows < 1 || cant_rows > 8)
+    {
+        cant_rows = get_int("How many rows do you want? (min=1,max=8) : ");
+    }
+    bool print_space = true;
+    for (int i = 1; i <= cant_rows; i++) 
+    {
+        for (int symbol = 0; symbol < i; symbol++) 
+        {
+            if (print_space == true) 
+            {
+                for (int space = cant_rows; space > i; space--) 
+                {
+                    printf(" ");
+                }
+                print_space = false;
+            }
+            printf("#");
+        }
+        printf("  ");
+        for (int symbol = 0; symbol < i; symbol++) 
+        {
+            printf("#");
+        }
+        printf("\n");
+        print_space = true;
+    }
+}
